8a6db7dba5d7a34a77142c4f628ce9b007143725
lumino <luminovoez@gmail.com>
2023-12-26 20:16:25 +0800

    myfetch

--- myfetch/response.go
+++ myfetch/response.go
@@ -1,3 +1,4 @@
+// timeline-deamon @ 2023-12-26
 // azure-go @ 2023-12-21
 
 package myfetch
@@ -10,8 +11,13 @@ import (
 )
 
 // this function receive json request.
-func ResponseToJson(r *http.Response) (*orderedmap.OrderedMap, error) {
-	o := orderedmap.New()
-	err := json.NewDecoder(r.Body).Decode(&o)
+func ResponseToObject(r *http.Response) (o *orderedmap.OrderedMap, err error) {
+	o = orderedmap.New()
+	err = json.NewDecoder(r.Body).Decode(&o)
 	return o, err
+}
+
+func ResponseToObjectArray(r *http.Response) (arr []*orderedmap.OrderedMap, err error) {
+	err = json.NewDecoder(r.Body).Decode(&arr)
+	return arr, err
 }
