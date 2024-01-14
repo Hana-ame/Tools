1fd708c52bfb19129d8dfa1e2d47fec1aa78f8c3
lumino <luminovoez@gmail.com>
2024-01-15 05:00:10 +0800

    import something

--- Utils.java
+++ Utils.java
@@ -1,9 +1,11 @@
+import java.util.List;
+
 class Utils {
     public static int gcd(int a, int b) {
         if (b == 0) return a;
         return gcd(b, a%b);
     }
-    public static void printList(List a) {
+    public static void printList(List<?> a) {
         for (Object o : a) {
             System.out.printf("%s, ", o.toString());            
         }
