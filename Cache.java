b7fb53579acf98c6943789e96821ca0bf3f4b9d0
lumino <luminovoez@gmail.com>
2024-01-24 15:27:00 +0800

    add example for cache

--- Cache.java
+++ Cache.java
@@ -1,27 +1,99 @@
 import java.util.HashMap;
 
-class Cache <T> {
-  HashMap<String, T> m;
-  Cache(){
+class Cache<T> {
+  HashMap<Function<T>, T> m;
+
+  Cache() {
     m = new HashMap<>();
   }
 
-  public T handle(Function<T> Function){
-    if (m.containsKey(Function.hashString())) {
-      return m.get(Function.hashString());
+  public T run(Function<T> function) {
+    if (m.containsKey(function)) {
+      return m.get(function);
     }
-    T result = Function.run();
-    m.put(Function.hashString(), result);
+    T result = function.run();
+    m.put(function, result);
     return result;
   }
 
-  public void override(Function<T> Function, T result) {
-    m.put(Function.hashString(), result);
+  public void override(Function<T> function, T result) {
+    m.put(function, result);
   }
-  public T override(Function<T> Function) {
-    T result = Function.run();
-    m.put(Function.hashString(), result);
+
+  public T override(Function<T> function) {
+    T result = function.run();
+    m.put(function, result);
     return result;
   }
 
+  public static void main(String[] args) {
+    Cache<String> cache = new Cache<>();
+    String r = null;
+    r = cache.run(new MyFun(1, 2, 'a', "null"));
+    System.out.println(r);
+    r = cache.run(new MyFun(1, 2, 'a', "nul2l"));
+    System.out.println(r);
+    r = cache.run(new MyFun(2, 2, 'a', "nul3l"));
+    System.out.println(r);
+    r = cache.run(new MyFun(2, 2, 'a', "nul4l"));
+    System.out.println(r);
+    // null
+    // null 
+    // nul3l
+    // nul3l
+
+    // 不行，可能是因为有hashcode。
+    // int a = 1;
+    // int b = 2;
+    // cache.run(() -> {
+    //    " a "
+    // });
+
+  }
+
+}
+
+class MyFun implements Function<String> {
+
+  int a;
+  int b;
+  char c;
+  String s;
+
+  MyFun(int a, int b, char c, String s) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    this.s = s;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + a;
+    result = prime * result + b;
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    MyFun other = (MyFun) obj;
+    if (a != other.a)
+      return false;
+    if (b != other.b)
+      return false;
+    return true;
+  }
+
+  @Override
+  public String run() {
+    return s;
+  }
 }
--- Utils.java
+++ Utils.java
@@ -1,19 +1,22 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 class Utils {
     // math
     public static int gcd(int a, int b) {
-        if (b == 0) return a;
-        return gcd(b, a%b);
+        if (b == 0)
+            return a;
+        return gcd(b, a % b);
+        Comparator<T>.class;
     }
 
     // builder
     public static String toRawString(String s) {
         int n = s.length();
         StringBuilder sb = new StringBuilder();
-        for (int i=0; i<n; i++) {
+        for (int i = 0; i < n; i++) {
             char ch = s.charAt(i);
             if (".$|()[]{}^?*+\\".indexOf(ch) != -1) {
                 sb.append('\\');
@@ -31,30 +34,43 @@ class Utils {
         return list;
     }
 
+    public static ArrayList<Character> asList(char[] arr) {
+        ArrayList<Character> list = new ArrayList<>(arr.length);
+        for (char i : arr)
+            list.add(i);
+        return list;
+    }
+
+    public static <T> ArrayList<T> asList(T[] arr) {
+        List<T> list = Arrays.asList(arr);
+        return (ArrayList<T>) list;
+    }
 
     // print
     public static void printList(List<?> a) {
         for (Object o : a) {
-            System.out.printf("%s, ", o.toString());            
+            System.out.printf("%s, ", o.toString());
         }
         System.out.println();
     }
 
     public static void printArray(Object[] a) {
         for (Object o : a) {
-            System.out.printf("%s, ", o.toString());            
+            System.out.printf("%s, ", o.toString());
         }
         System.out.println();
-    }    
+    }
+
     public static void printArray(char[] a) {
         for (char o : a) {
-            System.out.printf("%s, ", o);            
+            System.out.printf("%s, ", o);
         }
         System.out.println();
-    }    
+    }
+
     public static void printArray(int[] a) {
         for (int o : a) {
-            System.out.printf("%s, ", o);            
+            System.out.printf("%s, ", o);
         }
         System.out.println();
     }
