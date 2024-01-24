53ea03ec9481555a869dc60bc78f8cd1598c727e
lumino <luminovoez@gmail.com>
2024-01-24 15:37:14 +0800

    generate test.

---
+++ UtilsTest.java
@@ -0,0 +1,48 @@
+import org.junit.jupiter.api.Test;
+
+public class UtilsTest {
+  @Test
+  void testAsList() {
+
+  }
+
+  @Test
+  void testAsList2() {
+
+  }
+
+  @Test
+  void testAsList3() {
+
+  }
+
+  @Test
+  void testGcd() {
+
+  }
+
+  @Test
+  void testPrintArray() {
+
+  }
+
+  @Test
+  void testPrintArray2() {
+
+  }
+
+  @Test
+  void testPrintArray3() {
+
+  }
+
+  @Test
+  void testPrintList() {
+
+  }
+
+  @Test
+  void testToRawString() {
+
+  }
+}
