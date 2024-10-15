9934c3ce8ce12d3d27a99eee30aaa7159e33e35b
lumin <luminovoez@gmail.com>
2024-10-16 01:23:02 +0800

    add agent for my_curl

--- json.go
+++ json.go
@@ -3,8 +3,11 @@
 package tools
 
 import (
+	"bytes"
 	"encoding/json"
+	"io"
 	"os"
+	"strings"
 
 	"github.com/Hana-ame/fedi-antenna/Tools/orderedmap"
 )
@@ -18,4 +21,27 @@ func ReadJsonFile(fn string) (*orderedmap.OrderedMap, error) {
 	o := orderedmap.New()
 	err = json.NewDecoder(jsonFile).Decode(&o)
 	return o, err
+}
+
+
+func ReaderToJson(reader io.Reader) (*orderedmap.OrderedMap, error) {
+	o := orderedmap.New()
+	err := json.NewDecoder(reader).Decode(&o)
+	return o, err
+}
+
+func StringToJson(s string) (*orderedmap.OrderedMap, error) {
+	return ReaderToJson(strings.NewReader(s))
+}
+
+func BytesToJson(b []byte) (*orderedmap.OrderedMap, error) {
+	return ReaderToJson(bytes.NewReader(b))
+}
+
+func OpenFileToJson(fn string) (*orderedmap.OrderedMap, error) {
+	f, err := os.Open(fn)
+	if err != nil {
+		return nil, err
+	}
+	return ReaderToJson(f)
 }
