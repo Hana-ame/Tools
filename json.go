87981e3fcbbf83efbfb6aab66909535b84feba71
lumino <luminovoez@gmail.com>
2024-11-13 13:06:24 +0800

    file

--- json.go
+++ json.go
@@ -13,7 +13,7 @@ import (
 )
 
 // this function receive json request.
-func ReadJsonFile(fn string) (*orderedmap.OrderedMap, error) {
+func ReadJSONFile(fn string) (*orderedmap.OrderedMap, error) {
 	jsonFile, err := os.Open(fn)
 	if err != nil {
 		return nil, err
@@ -23,24 +23,24 @@ func ReadJsonFile(fn string) (*orderedmap.OrderedMap, error) {
 	return o, err
 }
 
-func ReaderToJson(reader io.Reader) (*orderedmap.OrderedMap, error) {
+func ReaderToJSON(reader io.Reader) (*orderedmap.OrderedMap, error) {
 	o := orderedmap.New()
 	err := json.NewDecoder(reader).Decode(&o)
 	return o, err
 }
 
-func StringToJson(s string) (*orderedmap.OrderedMap, error) {
-	return ReaderToJson(strings.NewReader(s))
+func StringToJSON(s string) (*orderedmap.OrderedMap, error) {
+	return ReaderToJSON(strings.NewReader(s))
 }
 
-func BytesToJson(b []byte) (*orderedmap.OrderedMap, error) {
-	return ReaderToJson(bytes.NewReader(b))
+func BytesToJSON(b []byte) (*orderedmap.OrderedMap, error) {
+	return ReaderToJSON(bytes.NewReader(b))
 }
 
-func ReadFileToJson(fn string) (*orderedmap.OrderedMap, error) {
+func ReadFileToJSON(fn string) (*orderedmap.OrderedMap, error) {
 	f, err := os.Open(fn)
 	if err != nil {
 		return nil, err
 	}
-	return ReaderToJson(f)
+	return ReaderToJSON(f)
 }
