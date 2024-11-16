53cdf792d96ab39a470c5bba0460a0f711f64be4
lumin <luminovoez@gmail.com>
2024-11-17 00:05:07 +0800

    add comments

--- utils.ts
+++ utils.ts
@@ -6,16 +6,25 @@ export function truncateString(str: string, num: number) {
   return str.slice(0, num) + '...';
 };
 
+
+
 // 24-11-16：这个函数能够对准endpoint代理一个需要CORS的请求。这里是为了得到bilibili封面用的。
-const Origin = "https://proxy.moonchan.xyz"
+// "X-Content-Type",
+// "X-Content-Length",
+// "X-Host",
+// "X-Origin",
+// "X-Referer",
+// "X-Cookie",
+// 这些会替换那些不让传的，Host是这里设定的
+const END_POINT = "https://proxy.moonchan.xyz"
 export function fetchWithProxy(
   input: string | URL,
   init?: RequestInit,
 ): Promise<Response> {
   const url = new URL(input);
-  const endpoint = Origin + url.pathname + url.search
+  const endpoint = END_POINT + url.pathname + url.search
 
-  if (!init || !init.headers) {
+  if (!init) {
     return fetch(endpoint, {
       headers: {
         "X-Host": url.hostname,
@@ -23,11 +32,17 @@ export function fetchWithProxy(
     });
   }
 
+  if (!init.headers) {
+    init.headers = {
+      "X-Host": url.hostname,
+    };
+    return fetch(endpoint, init);
+  }
+
   // 如果 init 和 init.headers 存在，将 X-Host 加入到现有的 headers 中
   if (init.headers instanceof Headers) {
-      init.headers.set("X-Host", url.hostname);
+    init.headers.set("X-Host", url.hostname);
   } 
 
   return fetch(endpoint, init); // 使用更新后的 init 进行 fetch
-
 }
