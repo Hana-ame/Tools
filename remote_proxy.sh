99a4f2ab2a6839b55232cc0ba3cb81e1964fd1ad
lumin <luminovoez@gmail.com>
2024-11-17 20:22:19 +0800

    remote_proxy.sh

--- remote_proxy.sh
+++ remote_proxy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-
+source ~/script/env.source
 while true
 do
-    sleep 5
-    ssh -o "ProxyCommand=nc -X 5 -x $SOCKS_PROXY %h %p" root@bwh.moonchan.xyz -p26275 "~/script/kill_ssh.sh"
-    sleep 5
-    ssh -o "ProxyCommand=nc -X 5 -x $SOCKS_PROXY %h %p" root@bwh.moonchan.xyz -p26275 \
+    (ssh -o "ProxyCommand=nc -X 5 -x $SOCKS_PROXY %h %p" root@bwh.moonchan.xyz -p26275 "~/script/kill_ssh.sh") \
+    && \
+    (ssh -o "ProxyCommand=nc -X 5 -x $SOCKS_PROXY %h %p" root@bwh.moonchan.xyz -p26275 \
 	    -R [2001:470:c:6c::3]:22:localhost:22 \
 	    -R [2001:470:c:6c::3]:3000:localhost:3000 \
-	    -o ServerAliveInterval=30
+	    -o ServerAliveInterval=3
+    )
     sleep 5
 done
