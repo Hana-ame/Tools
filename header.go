8056b0ad5af64b264cd9c4c47b63307bde89deb4
lumin <luminovoez@gmail.com>
2024-11-21 19:50:03 +0800

    Access-Control-Expose-Headers

--- header.go
+++ header.go
@@ -13,6 +13,14 @@ func (h Header) Add(key, value string) {
 	h.Header.Add(key, value)
 }
 
+func (h Header) GetAllKeys() []string {
+	s := make([]string, len(h.Header))
+	for k := range h.Header {
+		s = append(s, k)
+	}
+	return s
+}
+
 func NewHeader(header http.Header) Header {
 	if header == nil {
 		header = http.Header{}
