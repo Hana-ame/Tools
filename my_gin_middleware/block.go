cd794cdf0f43404ec6b0ae960e6ab9f2c6379a2b
lumin <luminovoez@gmail.com>
2024-11-23 16:16:21 +0800

    可能换种方式好点

--- my_gin_middleware/block.go
+++ my_gin_middleware/block.go
@@ -31,3 +31,31 @@ func BlockMiddleware() gin.HandlerFunc {
 		c.Next() // 继续处理请求
 	}
 }
+
+func RedirectMiddleware(host string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		if host == "" {
+			c.Next()
+			return
+		}
+
+		// 获取请求中的 Cookie
+		cookie, err := c.Cookie("pass")
+
+		// 如果 Cookie 包含 pass=pass，直接继续处理请求
+		if err == nil && cookie == "pass" {
+			c.Next() // 继续处理请求
+			return
+		}
+
+		// 如果不在 "CN", "" 中的任意一个。
+		if !slices.Contains([]string{"CN", ""}, c.Request.Header.Get("Cf-Ipcountry")) {
+			path := c.Request.URL.String()
+			c.Redirect(http.StatusFound, "https://"+host+path)
+			return
+		}
+
+		c.Next() // 继续处理请求
+	}
+}
