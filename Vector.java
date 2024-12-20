b78a02c00f601220ccfde55eaa2dc56e9111f77d
Hana Amelia <Luminovoez@gmail.com>
2024-12-20 22:11:38 +0800

    Update Vector.java

--- Vector.java
+++ Vector.java
@@ -12,10 +12,18 @@ public class IntVector {
     }
 
     public IntVector add(IntVector v) {
-        if (data.length == v.data.length) {
-            for (int i=0; i<data.length; i++) {
-                data[i] += v.data[i];
-            }
+        int minLength = Math.min(data.length, v.data.length);
+        for (int i = 0; i < minLength; i++) {
+            data[i] += v.data[i];
+        }
+        return this;
+    }
+
+    
+    public IntVector minus(IntVector v) {
+        int minLength = Math.min(data.length, v.data.length);
+        for (int i = 0; i < minLength; i++) {
+            data[i] -= v.data[i];
         }
         return this;
     }
@@ -29,6 +37,30 @@ public class IntVector {
             throw new IndexOutOfBoundsException("Index out of bounds");
         }
         return data[index];
+    }
+
+    public static IntVector add(IntVector v1, IntVector v2) {
+        int maxLength = Math.max(v1.size(), v2.size());
+        int[] result = new int[maxLength];
+
+        for (int i = 0; i < maxLength; i++) {
+            int val1 = (i < v1.size()) ? v1.get(i) : 0;
+            int val2 = (i < v2.size()) ? v2.get(i) : 0;
+            result[i] = val1 + val2;
+        }
+        return new IntVector(result);
+    }
+
+    public static IntVector minus(IntVector v1, IntVector v2) {
+        int maxLength = Math.max(v1.size(), v2.size());
+        int[] result = new int[maxLength];
+
+        for (int i = 0; i < maxLength; i++) {
+            int val1 = (i < v1.size()) ? v1.get(i) : 0;
+            int val2 = (i < v2.size()) ? v2.get(i) : 0;
+            result[i] = val1 - val2;
+        }
+        return new IntVector(result);
     }
 
 }
