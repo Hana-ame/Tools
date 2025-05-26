585a4c739cbd46740a9ae089dca1aa754ea11f84
lumin <luminovoez@gmail.com>
2025-05-27 07:40:15 +0800

    sqlite

--- image_test.go
+++ image_test.go
@@ -4,9 +4,13 @@ package tools
 
 import (
 	"fmt"
+	"image"
 	"log"
 	"net/http"
+	"os"
 	"testing"
+
+	"golang.org/x/image/webp"
 )
 
 func TestRespToImg(t *testing.T) {
@@ -15,15 +19,34 @@ func TestRespToImg(t *testing.T) {
 	// For example, to test, you could set up a mock server or fetch a real image.
 
 	// Example: Fetching a known WebP image (replace with a real URL if testing)
-	resp, err := http.Get("https://www.gstatic.com/webp/gallery/1.webp")
+	resp, err := http.Get("https://upload.moonchan.xyz/api/01LLWEUU2LK7VKGI67L5GKX5ZBW7OKKQYZ/output_001.webp")
 	if err != nil {
 		log.Fatalf("Failed to get image: %v", err)
 	}
+	defer resp.Body.Close()
 
-	img, err := DecodeResponseToImage(resp)
+	img, err := webp.Decode(resp.Body)
 	if err != nil {
 		log.Fatalf("Failed to decode image: %v", err)
 	}
 
+	fmt.Printf("Successfully decoded image. Dimensions: %s\n", img.Bounds())
+}
+
+func TestWebp(t *testing.T) {
+	webpFile, err := os.Open("/mnt/c//Users/lumin/Downloads/output_001.webp")
+	if err != nil {
+		log.Fatalf("err: %v", err)
+	}
+	img, err := webp.Decode(webpFile)
+	if err != nil {
+		log.Fatalf("err: %v", err)
+	}
+	fmt.Printf("Successfully decoded image. Dimensions: %s\n", img.Bounds())
+	img, fn, err := image.Decode(webpFile) // The second return value is the format string
+	if err != nil {
+		log.Fatalf("unsupported or malformed image format for Content-Type '%s': [%s]", err, fn)
+	}
+	fmt.Printf("Successfully decoded image. Dimensions: %s\n", fn)
 	fmt.Printf("Successfully decoded image. Dimensions: %s\n", img.Bounds())
 }
