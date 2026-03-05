cc08855c3cabe5f9c99d99918c3e4283296db6f1
lumin <luminovoez@gmail.com>
2026-03-05 13:04:41 +0800

    debian13, bandwagon

--- bwh/install.sh
+++ bwh/install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-# curl -sL https://raw.githubusercontent.com/Hana-ame/Tools/refs/heads/script/bwh/install.sh | bash
+# curl -fsSL https://raw.githubusercontent.com/Hana-ame/Tools/refs/heads/script/bwh/install.sh | bash
 
 # 这一段是让你在console里面运行的。
 apt update -y && apt upgrade -y
 apt install git -y
+apt install curl -y
+apt install sudo -y
 # # 确保~/.ssh目录存在
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
@@ -23,11 +25,11 @@ echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCwF4AUmXUJ6eC14jNfe6gTBV3iYvMr6RHuCt
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDMdcbLQK62FfDMNo1wjAgw6VlUugmxBrID+ijeHnxRhwMA6Ld/yjQ0Ean338wJoLq7GmfuTlsMocmCVubKNDnG0nyg4M8jkklTZZ76aYp0bAzufqXmkItWMU3uyqWyQDEqJdVKc1OEOY4JGaD+47y7nLWzlsOY0DwBW6BvQrA425fQPhi75KLPn5VBHniikhxoNZdSkywZ+2+4j1eoCpSK+3oRKG3TqdoPONZCjLRJxlgQ9kTgp4BJbBRBSFIv+uXYoNtv/k0B80QK5rIiy6xuns0McQUOfQ/GdyZXt8PNw2YcyqS+u+YkFl32EovPjMP1UB+XjswDvgMtgDep18wN" >>  ~/.ssh/authorized_keys
 
 # 1. 禁用密码验证
-sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication no/' /etc/ssh/sshd_config
+sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication no/' /etc/ssh/sshd_config
 
 # 2. 禁用挑战响应验证（防止备用密码登录方式）
-sudo sed -i 's/^#\?ChallengeResponseAuthentication.*/ChallengeResponseAuthentication no/' /etc/ssh/sshd_config
-sudo sed -i 's/^#\?KbdInteractiveAuthentication.*/KbdInteractiveAuthentication no/' /etc/ssh/sshd_config
+sed -i 's/^#\?ChallengeResponseAuthentication.*/ChallengeResponseAuthentication no/' /etc/ssh/sshd_config
+sed -i 's/^#\?KbdInteractiveAuthentication.*/KbdInteractiveAuthentication no/' /etc/ssh/sshd_config
 
 cd ~;
 
@@ -41,13 +43,13 @@ NEW_JOB="@reboot sleep 60;~/script/bwh/startup.sh";
 
 
 # 关闭交换文件
-sudo swapoff /swapfile
+sudo swapoff /swap
 
 # 删除原文件
-sudo rm /swapfile
+sudo rm /swap
 
 # 重新创建新大小的文件（步骤同上）
-sudo fallocate -l 2G /swapfile
-sudo chmod 600 /swapfile
-sudo mkswap /swapfile
-sudo swapon /swapfile
+sudo fallocate -l 2G /swap
+sudo chmod 600 /swap
+sudo mkswap /swap
+sudo swapon /swap
