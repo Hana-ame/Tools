d229705a2aba417d70aabd3c92aa98ecaa82e12e
lumin <luminovoez@gmail.com>
2026-03-14 00:51:28 +0800

    cloudcone ipv6

--- cloudcone/net6.sh
+++ cloudcone/net6.sh
@@ -4,5 +4,7 @@
 /sbin/ifconfig sit0 up
 /sbin/ifconfig sit0 inet6 tunnel ::66.220.18.42
 /sbin/ifconfig sit1 up
-/sbin/route -A inet6 add ::/0 dev sit1
+/sbin/ip -6 route add ::/0 dev sit1 metric 2048
 /sbin/ip link set dev sit1 mtu 1280
+
+/sbin/ip addr add 2001:470:c:6c::2 dev sit1
