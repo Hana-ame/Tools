8fd4d59de702120bedc3b185b2352ae92ae25ef5
Nanaka <luminovoez@gmail.com>
2026-08-11 19:08:50 +0800

    fix(ssh/bwh): pass -p26275 in command mode so ssh uses the correct port (previously hung on port 22)

--- ssh/bwh.sh
+++ ssh/bwh.sh
@@ -2,10 +2,10 @@
 
 if [ $# -eq 0 ]; then
     # No arguments: just connect to the host
-    ~/script/ssh/ssh.sh root@bwh.moonchan.xyz
+    ~/script/ssh/ssh.sh root@bwh.moonchan.xyz -p26275
 else
     # Split arguments: all but the last go before the host, the last goes after
     before=("${@:1:$#-1}")   # all arguments except the last
     last="${@: -1}"          # the last argument
-    ~/script/ssh/ssh.sh "${before[@]}" root@bwh.moonchan.xyz "$last"
+    ~/script/ssh/ssh.sh -p26275 "${before[@]}" root@bwh.moonchan.xyz "$last"
 fi
